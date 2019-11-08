f43cb89b5f5b03120ae4134e3a28ca1c3168e175
Stoyan Kalinov <bloodyrax@gmail.com>
2019-11-08 22:55:26 +0200

    Fourth line

--- index.php
+++ index.php
@@ -2,3 +2,4 @@
 echo "TEST";
 echo "Second TEST from local branch";
 echo "Third TEST for the second remote branch";
+echo "Fourth test for the default remote branch which is not set as 'tracked branch'";
